dbf4fa727ad30e7cf2ab9cf1a1879caf5d0ca568
Zhengyi Luo <zhengyil@nvidia.com>
2023-02-10 11:54:34 -0500

    webcam

---
+++ scripts/demo_speed.py
@@ -0,0 +1,109 @@
+"""Image demo script."""
+import argparse
+import os
+import pickle as pk
+
+import cv2
+import numpy as np
+import torch
+from easydict import EasyDict as edict
+from torchvision import transforms as T
+from tqdm import tqdm
+
+from hybrik.models import builder
+from hybrik.utils.config import update_config
+from hybrik.utils.presets import SimpleTransform3DSMPLCam
+from hybrik.utils.vis import get_max_iou_box, get_one_box, vis_2d
+import time
+
+det_transform = T.Compose([T.ToTensor()])
+
+
+def xyxy2xywh(bbox):
+    x1, y1, x2, y2 = bbox
+
+    cx = (x1 + x2) / 2
+    cy = (y1 + y2) / 2
+    w = x2 - x1
+    h = y2 - y1
+    return [cx, cy, w, h]
+
+
+cfg_file = 'configs/256x192_adam_lr1e-3-res34_smpl_3d_cam_2x_mix_w_pw3d_fast.yaml'
+CKPT = './pretrained_w_cam.pth'
+cfg = update_config(cfg_file)
+
+bbox_3d_shape = getattr(cfg.MODEL, 'BBOX_3D_SHAPE', (2000, 2000, 2000))
+bbox_3d_shape = [item * 1e-3 for item in bbox_3d_shape]
+dummpy_set = edict({'joint_pairs_17': None, 'joint_pairs_24': None, 'joint_pairs_29': None, 'bbox_3d_shape': bbox_3d_shape})
+
+transformation = SimpleTransform3DSMPLCam(dummpy_set,
+                                          scale_factor=cfg.DATASET.SCALE_FACTOR,
+                                          color_factor=cfg.DATASET.COLOR_FACTOR,
+                                          occlusion=cfg.DATASET.OCCLUSION,
+                                          input_size=cfg.MODEL.IMAGE_SIZE,
+                                          output_size=cfg.MODEL.HEATMAP_SIZE,
+                                          depth_dim=cfg.MODEL.EXTRA.DEPTH_DIM,
+                                          bbox_3d_shape=bbox_3d_shape,
+                                          rot=cfg.DATASET.ROT_FACTOR,
+                                          sigma=cfg.MODEL.EXTRA.SIGMA,
+                                          train=False,
+                                          add_dpg=False,
+                                          loss_type=cfg.LOSS['TYPE'])
+
+det_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+det_model.classes = [0]
+
+hybrik_model = builder.build_sppe(cfg.MODEL)
+
+print(f'Loading model from {CKPT}...')
+save_dict = torch.load(CKPT, map_location='cpu')
+if type(save_dict) == dict:
+    model_dict = save_dict['model']
+    hybrik_model.load_state_dict(model_dict)
+else:
+    hybrik_model.load_state_dict(save_dict)
+det_model.cuda()
+hybrik_model.cuda()
+det_model.eval()
+hybrik_model.eval()
+device = torch.device('cuda')
+
+
+def frames_from_webcam():
+    cap = cv2.VideoCapture(0)
+    while (frame_bgr := cap.read()[1]) is not None:
+        yield frame_bgr[..., ::-1]
+
+
+print('### Run Model...')
+idx = 0
+for frame in frames_from_webcam():
+
+    with torch.no_grad():
+        # Run Detection
+
+        t_s = time.time()
+
+        yolo_output = det_model([frame], size=360)
+
+        yolo_out_xyxy = torch.stack(yolo_output.xyxy)
+        det_output = {"boxes": yolo_out_xyxy[:, 0, :4], "scores": yolo_out_xyxy[:, 0, 4]}
+
+        if prev_box is None:
+            tight_bbox = get_one_box(det_output)  # xyxy
+            if tight_bbox is None:
+                continue
+        else:
+            tight_bbox = get_max_iou_box(det_output, prev_box)  # xyxy
+
+        prev_box = tight_bbox
+
+        # Run HybrIK
+        # bbox: [x1, y1, x2, y2]
+        pose_input, bbox, img_center = transformation.test_transform(frame, tight_bbox)
+        pose_input = pose_input.to(device)[None, :, :, :]
+
+        pose_output = hybrik_model(pose_input, flip_test=False, bboxes=torch.from_numpy(np.array(bbox)).to(pose_input.device).unsqueeze(0).float(), img_center=torch.from_numpy(img_center).to(pose_input.device).unsqueeze(0).float())
+        dt = time.time() - t_s
+        print(1 / dt)
--- scripts/demo_webcam.py
+++ scripts/demo_webcam.py
@@ -8,13 +8,14 @@ import numpy as np
 import torch
 from easydict import EasyDict as edict
 from torchvision import transforms as T
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
 from tqdm import tqdm
 
 from hybrik.models import builder
 from hybrik.utils.config import update_config
 from hybrik.utils.presets import SimpleTransform3DSMPLCam
+from hybrik.utils.render_pytorch3d import render_mesh
 from hybrik.utils.vis import get_max_iou_box, get_one_box, vis_2d
-import time
 
 det_transform = T.Compose([T.ToTensor()])
 
@@ -29,6 +30,49 @@ def xyxy2xywh(bbox):
     return [cx, cy, w, h]
 
 
+def get_video_info(in_file):
+    stream = cv2.VideoCapture(in_file)
+    assert stream.isOpened(), 'Cannot capture source'
+    # self.path = input_source
+    datalen = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
+    fourcc = int(stream.get(cv2.CAP_PROP_FOURCC))
+    fps = stream.get(cv2.CAP_PROP_FPS)
+    frameSize = (int(stream.get(cv2.CAP_PROP_FRAME_WIDTH)), int(stream.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    # bitrate = int(stream.get(cv2.CAP_PROP_BITRATE))
+    videoinfo = {'fourcc': fourcc, 'fps': fps, 'frameSize': frameSize}
+    stream.release()
+
+    return stream, videoinfo, datalen
+
+
+def recognize_video_ext(ext=''):
+    if ext == 'mp4':
+        return cv2.VideoWriter_fourcc(*'mp4v'), '.' + ext
+    elif ext == 'avi':
+        return cv2.VideoWriter_fourcc(*'XVID'), '.' + ext
+    elif ext == 'mov':
+        return cv2.VideoWriter_fourcc(*'XVID'), '.' + ext
+    else:
+        print("Unknow video format {}, will use .mp4 instead of it".format(ext))
+        return cv2.VideoWriter_fourcc(*'mp4v'), '.mp4'
+
+
+parser = argparse.ArgumentParser(description='HybrIK Demo')
+
+parser.add_argument('--gpu', help='gpu', default=0, type=int)
+# parser.add_argument('--img-path',
+#                     help='image name',
+#                     default='',
+#                     type=str)
+parser.add_argument('--video-name', help='video name', default='', type=str)
+parser.add_argument('--out-dir', help='output folder', default='', type=str)
+parser.add_argument('--save-pk', default=False, dest='save_pk', help='save prediction', action='store_true')
+parser.add_argument('--save-img', default=False, dest='save_img', help='save prediction', action='store_true')
+
+opt = parser.parse_args()
+
+# CKPT = './pretrained_hrnet.pth'
+# cfg_file = 'configs/256x192_adam_lr1e-3-res34_smpl_3d_cam_2x_mix_w_pw3d.yaml'
 cfg_file = 'configs/256x192_adam_lr1e-3-res34_smpl_3d_cam_2x_mix_w_pw3d_fast.yaml'
 CKPT = './pretrained_w_cam.pth'
 cfg = update_config(cfg_file)
@@ -36,6 +80,28 @@ cfg = update_config(cfg_file)
 bbox_3d_shape = getattr(cfg.MODEL, 'BBOX_3D_SHAPE', (2000, 2000, 2000))
 bbox_3d_shape = [item * 1e-3 for item in bbox_3d_shape]
 dummpy_set = edict({'joint_pairs_17': None, 'joint_pairs_24': None, 'joint_pairs_29': None, 'bbox_3d_shape': bbox_3d_shape})
+
+res_keys = [
+    'pred_uvd',
+    'pred_xyz_17',
+    'pred_xyz_29',
+    'pred_xyz_24_struct',
+    'pred_scores',
+    # 'pred_camera',
+    # 'f',
+    'pred_betas',
+    'pred_thetas',
+    'pred_phi',
+    'pred_cam_root',
+    # 'features',
+    'transl',
+    'transl_camsys',
+    'bbox',
+    'height',
+    'width',
+    'img_path'
+]
+res_db = {k: [] for k in res_keys}
 
 transformation = SimpleTransform3DSMPLCam(dummpy_set,
                                           scale_factor=cfg.DATASET.SCALE_FACTOR,
@@ -51,6 +117,7 @@ transformation = SimpleTransform3DSMPLCam(dummpy_set,
                                           add_dpg=False,
                                           loss_type=cfg.LOSS['TYPE'])
 
+# det_model = fasterrcnn_resnet50_fpn(pretrained=True)
 det_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 det_model.classes = [0]
 
@@ -63,47 +130,97 @@ if type(save_dict) == dict:
     hybrik_model.load_state_dict(model_dict)
 else:
     hybrik_model.load_state_dict(save_dict)
-det_model.cuda()
-hybrik_model.cuda()
+
+det_model.cuda(opt.gpu)
+hybrik_model.cuda(opt.gpu)
 det_model.eval()
 hybrik_model.eval()
-device = torch.device('cuda')
 
+print('### Extract Image...')
+video_basename = os.path.basename(opt.video_name).split('.')[0]
 
-def frames_from_webcam():
-    cap = cv2.VideoCapture(0)
-    while (frame_bgr := cap.read()[1]) is not None:
-        yield frame_bgr[..., ::-1]
+if not os.path.exists(opt.out_dir):
+    os.makedirs(opt.out_dir)
+if not os.path.exists(os.path.join(opt.out_dir, 'raw_images')):
+    os.makedirs(os.path.join(opt.out_dir, 'raw_images'))
+if not os.path.exists(os.path.join(opt.out_dir, 'res_images')) and opt.save_img:
+    os.makedirs(os.path.join(opt.out_dir, 'res_images'))
+if not os.path.exists(os.path.join(opt.out_dir, 'res_2d_images')) and opt.save_img:
+    os.makedirs(os.path.join(opt.out_dir, 'res_2d_images'))
 
+_, info, _ = get_video_info(opt.video_name)
+video_basename = os.path.basename(opt.video_name).split('.')[0]
+
+savepath = f'./{opt.out_dir}/res_{video_basename}.mp4'
+savepath2d = f'./{opt.out_dir}/res_2d_{video_basename}.mp4'
+info['savepath'] = savepath
+info['savepath2d'] = savepath2d
+
+write_stream = cv2.VideoWriter(*[info[k] for k in ['savepath', 'fourcc', 'fps', 'frameSize']])
+write2d_stream = cv2.VideoWriter(*[info[k] for k in ['savepath2d', 'fourcc', 'fps', 'frameSize']])
+if not write_stream.isOpened():
+    print("Try to use other video encoders...")
+    ext = info['savepath'].split('.')[-1]
+    fourcc, _ext = recognize_video_ext(ext)
+    info['fourcc'] = fourcc
+    info['savepath'] = info['savepath'][:-4] + _ext
+    info['savepath2d'] = info['savepath2d'][:-4] + _ext
+    write_stream = cv2.VideoWriter(*[info[k] for k in ['savepath', 'fourcc', 'fps', 'frameSize']])
+    write2d_stream = cv2.VideoWriter(*[info[k] for k in ['savepath2d', 'fourcc', 'fps', 'frameSize']])
+
+assert write_stream.isOpened(), 'Cannot open video for writing'
+assert write2d_stream.isOpened(), 'Cannot open video for writing'
+
+os.system(f'ffmpeg -i {opt.video_name} {opt.out_dir}/raw_images/{video_basename}-%06d.png')
+
+files = os.listdir(f'{opt.out_dir}/raw_images')
+files.sort()
+
+img_path_list = []
+
+for file in tqdm(files):
+    if not os.path.isdir(file) and file[-4:] in ['.jpg', '.png']:
+
+        img_path = os.path.join(opt.out_dir, 'raw_images', file)
+        img_path_list.append(img_path)
+
+prev_box = None
+renderer = None
+smpl_faces = torch.from_numpy(hybrik_model.smpl.faces.astype(np.int32))
+import time
 
 print('### Run Model...')
 idx = 0
-for frame in frames_from_webcam():
+while True:
+    for img_path in tqdm(img_path_list):
+        dirname = os.path.dirname(img_path)
+        basename = os.path.basename(img_path)
 
-    with torch.no_grad():
-        # Run Detection
+        with torch.no_grad():
+            # Run Detection
 
-        t_s = time.time()
+            input_image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+            t_s = time.time()
 
-        yolo_output = det_model([frame], size=360)
+            yolo_output = det_model([input_image], size=360)
 
-        yolo_out_xyxy = torch.stack(yolo_output.xyxy)
-        det_output = {"boxes": yolo_out_xyxy[:, 0, :4], "scores": yolo_out_xyxy[:, 0, 4]}
+            yolo_out_xyxy = torch.stack(yolo_output.xyxy)
+            det_output = {"boxes": yolo_out_xyxy[:, 0, :4], "scores": yolo_out_xyxy[:, 0, 4]}
 
-        if prev_box is None:
-            tight_bbox = get_one_box(det_output)  # xyxy
-            if tight_bbox is None:
-                continue
-        else:
-            tight_bbox = get_max_iou_box(det_output, prev_box)  # xyxy
+            if prev_box is None:
+                tight_bbox = get_one_box(det_output)  # xyxy
+                if tight_bbox is None:
+                    continue
+            else:
+                tight_bbox = get_max_iou_box(det_output, prev_box)  # xyxy
 
-        prev_box = tight_bbox
+            prev_box = tight_bbox
 
-        # Run HybrIK
-        # bbox: [x1, y1, x2, y2]
-        pose_input, bbox, img_center = transformation.test_transform(frame, tight_bbox)
-        pose_input = pose_input.to(device)[None, :, :, :]
+            # Run HybrIK
+            # bbox: [x1, y1, x2, y2]
+            pose_input, bbox, img_center = transformation.test_transform(input_image, tight_bbox)
+            pose_input = pose_input.to(opt.gpu)[None, :, :, :]
 
-        pose_output = hybrik_model(pose_input, flip_test=False, bboxes=torch.from_numpy(np.array(bbox)).to(pose_input.device).unsqueeze(0).float(), img_center=torch.from_numpy(img_center).to(pose_input.device).unsqueeze(0).float())
-        dt = time.time() - t_s
-        print(1 / dt)
+            pose_output = hybrik_model(pose_input, flip_test=False, bboxes=torch.from_numpy(np.array(bbox)).to(pose_input.device).unsqueeze(0).float(), img_center=torch.from_numpy(img_center).to(pose_input.device).unsqueeze(0).float())
+            dt = time.time() - t_s
+            print(1 / dt)
